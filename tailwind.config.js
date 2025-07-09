d74071f393f3f046a6abcba0ed65ce9d786d1596
unknown <MehmetDeviren1@hotmail.com>
2025-07-09 03:37:50 +0300

    fixed2

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  safelist: [
+    'bg-inherit',
+    'bg-transparent',
+    'bg-[#f6f6f6]',
+    'dark:bg-[#101624]'
+  ],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
